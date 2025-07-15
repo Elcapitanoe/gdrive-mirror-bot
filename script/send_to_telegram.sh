9ccb7534ac2751568a7411cfc37821302db9d7eb
Domi Adiwijaya <domy.adiwijaya@gmail.com>
2025-07-15 18:16:51 +0700

    Update send_to_telegram.sh

--- script/send_to_telegram.sh
+++ script/send_to_telegram.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 set -e
 
+# Ambil argumen
 GDRIVE_LINK="$1"
 FILE_NAME="$2"
 
-# Escape karakter spesial MarkdownV2
+# Fungsi untuk escape karakter MarkdownV2
 escape_md() {
-  echo "$1" | sed -E 's/([][_*()[\]~`>#+\-=|{}.!\\])/\\\1/g'
+  echo "$1" | sed -e 's/\\/\\\\/g' \
+                  -e 's/\./\\./g' \
+                  -e 's/\*/\\*/g' \
+                  -e 's/_/\\_/g' \
+                  -e 's/\[/\\[/g' \
+                  -e 's/\]/\\]/g' \
+                  -e 's/(/\\(/g' \
+                  -e 's/)/\\)/g' \
+                  -e 's/\~/\\~/g' \
+                  -e 's/\`/\\`/g' \
+                  -e 's/>/\\>/g' \
+                  -e 's/#/\\#/g' \
+                  -e 's/\+/\\+/g' \
+                  -e 's/-/\\-/g' \
+                  -e 's/=/\\=/g' \
+                  -e 's/{/\\{/g' \
+                  -e 's/}/\\}/g' \
+                  -e 's/!/\\!/g'
 }
 
+# Escape input agar tidak rusak saat dikirim ke Telegram
 ESCAPED_FILE_NAME=$(escape_md "$FILE_NAME")
 ESCAPED_GDRIVE_LINK=$(escape_md "$GDRIVE_LINK")
 
+# Format pesan
 MESSAGE="🚀 *New Mirror Uploaded!*\n\nFile Name: \`$ESCAPED_FILE_NAME\`\n\n🔗 [Download Here]($ESCAPED_GDRIVE_LINK)"
 
 # Kirim ke Telegram
