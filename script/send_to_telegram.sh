eb9350071df0e5d67d1f11f9b71d74a68e705e58
Domi Adiwijaya <domy.adiwijaya@gmail.com>
2025-07-15 15:17:07 +0700

    Update send_to_telegram.sh

--- script/send_to_telegram.sh
+++ script/send_to_telegram.sh
@@ -2,10 +2,12 @@
 set -e
 
 GDRIVE_LINK="$1"
-MESSAGE="🚀 *New Upload Available!*\n\n🔗 [Download Here]($GDRIVE_LINK)"
+FILE_NAME="$2"
+
+# Buat pesan dengan newline yang dirender Telegram
+MESSAGE=$(echo -e "🚀 *New Mirror Uploaded!*\n\nFile Name: $FILE_NAME\n\n🔗 [Download Here]($GDRIVE_LINK)")
 
 curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" \
   -d chat_id="${TELEGRAM_CHAT_ID}" \
   -d text="$MESSAGE" \
   -d parse_mode="Markdown"
-  
