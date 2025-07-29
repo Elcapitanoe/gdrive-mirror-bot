3fad0bf38c1a916c0b7e510d27a05f4645bea37c
Domi Adiwijaya <domy.adiwijaya@gmail.com>
2025-07-29 12:18:54 +0700

    feat(telegram): broadcast messages to private, group, and channel

--- script/send_to_telegram.sh
+++ script/send_to_telegram.sh
@@ -6,10 +6,18 @@ FILE_NAME="$2"
 
 MESSAGE="🚀 New Mirror Uploaded!
 
-File Name: $FILE_NAME
+📁 File Name: $FILE_NAME
 
 🔗 $GDRIVE_LINK"
 
 curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" \
-  -d chat_id="${TELEGRAM_CHAT_ID}" \
+  -d chat_id="${TELEGRAM_CHANNEL_ID}" \
+  -d text="$MESSAGE"
+
+curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" \
+  -d chat_id="${TELEGRAM_GROUP_ID}" \
+  -d text="$MESSAGE"
+
+curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" \
+  -d chat_id="${TELEGRAM_CHAT_PRIVATE_ID}" \
   -d text="$MESSAGE"
