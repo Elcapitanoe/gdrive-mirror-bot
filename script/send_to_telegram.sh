c6279cfa0894625adf8cdc3af79be908af97c780
Domi Adiwijaya <domy.adiwijaya@gmail.com>
2025-07-29 09:18:14 +0700

    chore: remove inline comments from script

--- script/send_to_telegram.sh
+++ script/send_to_telegram.sh
@@ -4,14 +4,12 @@ set -e
 GDRIVE_LINK="$1"
 FILE_NAME="$2"
 
-# Format pesan biasa (tanpa Markdown)
 MESSAGE="🚀 New Mirror Uploaded!
 
 File Name: $FILE_NAME
 
 🔗 $GDRIVE_LINK"
 
-# Kirim ke Telegram tanpa parse_mode
 curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" \
   -d chat_id="${TELEGRAM_CHAT_ID}" \
   -d text="$MESSAGE"
