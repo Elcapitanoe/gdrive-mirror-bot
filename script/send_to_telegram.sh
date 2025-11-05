f8d42e2dcd7f163bfd283794a976bd69482da83c
Domi Adiwijaya <domy.adiwijaya@gmail.com>
2025-11-05 08:23:40 +0700

    Update message format in send_to_telegram.sh

--- script/send_to_telegram.sh
+++ script/send_to_telegram.sh
@@ -4,11 +4,10 @@ set -e
 GDRIVE_LINK="$1"
 FILE_NAME="$2"
 
-MESSAGE="🚀 New Mirror Uploaded!
+MESSAGE="New Mirror Uploaded!
 
-📁 File Name: $FILE_NAME
-
-🔗 $GDRIVE_LINK"
+File Name: $FILE_NAME
+Url: $GDRIVE_LINK"
 
 curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" \
   -d chat_id="${TELEGRAM_CHANNEL_ID}" \
