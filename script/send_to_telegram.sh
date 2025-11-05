e239cc699ef822ab2c9f7b41617b370d03ba5e47
Domi Adiwijaya <domy.adiwijaya@gmail.com>
2025-11-05 08:36:13 +0700

    Change Telegram message button to 'Download File'

--- script/send_to_telegram.sh
+++ script/send_to_telegram.sh
@@ -9,7 +9,7 @@ BASE_TEXT=$(printf 'New Mirror Uploaded!\n\nFile Name: %s\n' "$FILE_NAME")
 GDRIVE_LINK_ESC=$(printf '%s' "$GDRIVE_LINK" | sed 's/"/\\"/g')
 
 for CHAT_ID in "$TELEGRAM_CHANNEL_ID" "$TELEGRAM_GROUP_ID" "$TELEGRAM_CHAT_PRIVATE_ID"; do
-  REPLY_MARKUP=$(printf '{"inline_keyboard":[[{"text":"Open in Google Drive","url":"%s"}]]}' "$GDRIVE_LINK_ESC")
+  REPLY_MARKUP=$(printf '{"inline_keyboard":[[{"text":"Download File","url":"%s"}]]}' "$GDRIVE_LINK_ESC")
 
   curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" \
     -d chat_id="$CHAT_ID" \
